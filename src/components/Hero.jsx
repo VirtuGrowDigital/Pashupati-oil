df6cb0a412ef55e470351a4e28b7f740a97d2678
VirtuGrow Digital <virtugrowdigital@gmail.com>
2025-10-11 12:36:50 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -60,8 +60,8 @@ const Hero = () => {
           </button>
 
           {/* WhatsApp Enquiry */}
-          <a
-            href={`https://wa.me/${9208468980}?text=Hi, I want to know more about your products`}
+            <a
+            href="https://wa.me/message/VTG5NXOH5TYIN1?text=Hi,%20I%20want%20to%20know%20more%20about%20your%20products"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-white/10 text-white text-sm sm:text-base md:text-lg font-extralight px-4 sm:px-6 md:px-8 py-2 sm:py-2.5 md:py-3 rounded-lg shadow hover:bg-green-700 transition cursor-pointer"
